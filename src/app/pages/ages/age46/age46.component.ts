2955a39688ee42d672af9ad907f53c9784881997
abouda199 <brahimabdelbari0@gmail.com>
2025-07-28 10:21:59 +0100

    Ajout des images et mise à jour des composants navbar, age46, age710

--- src/app/pages/ages/age46/age46.component.ts
+++ src/app/pages/ages/age46/age46.component.ts
@@ -5,4 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './age46.component.html',
   styleUrls: ['./age46.component.css']
 })
-export class Age46Component {}
+export class Age46Component {
+  selected: number | null = null;
+
+  toggle(index: number) {
+    this.selected = this.selected === index ? null : index;
+  }
+}
